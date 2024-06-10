e1faacb03149f540019008c5bdb029b3e8caf2c7
Nick T <me@nt3rp.io>
2024-06-10 00:10:21 -0400

    WIP

--- home/.chezmoiscripts/windows/run_after_11-install-fonts.ps1
+++ home/.chezmoiscripts/windows/run_after_11-install-fonts.ps1
@@ -1,6 +1,3 @@
-{{- if eq .chezmoi.os "windows" -}}
-# TODO: Find a better way to install fonts in Windows.
-
 # Get the Windows font directory.
 $fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
 
@@ -9,7 +6,6 @@ foreach ($file in Get-ChildItem $HOME/Fonts/*.ttf) {
     $fileName = $file.Name
     # ... But only if it isn't already installed.
     if (-not(Test-Path -Path "$HOME\AppData\Local\Microsoft\Windows\Fonts\$fileName" )) {
-        dir $file | %{ $fonts.CopyHere($_.fullname) }
+        Get-ChildItem $file | ForEach-Object { $fonts.CopyHere($_.fullname) }
     }
 }
-{{ end -}}
